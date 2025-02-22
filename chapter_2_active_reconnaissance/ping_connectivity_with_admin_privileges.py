dde515c095227475fe81c24c77cc6aa147c7ac26
TrystanW02 <trystanwilliams2002@outlook.com>
2025-02-22 14:39:44 -0600

    Delete chapter_2_active_reconnaissance directory

--- chapter_2_active_reconnaissance/ping_connectivity_with_admin_privileges.py
+++
@@ -1,9 +0,0 @@
-from pythonping import ping
-
-class PingConnectivity:
-    def check_ping_connectivity(self, host) -> None:
-        ping (host, verbose=True)
-        
-if __name__ == "__main__":
-    ping_check_connectivity = PingConnectivity()
-    ping_check_connectivity.check_ping_connectivity('google.com')
