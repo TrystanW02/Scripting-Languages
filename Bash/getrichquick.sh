cb28bda51ae21d8351fa5022e0bd4d853392cd59
Trystan Williams <trystanwilliams2002@outlook.com>
2025-03-26 10:26:30 -0500

    Update getrichquick.sh

--- Bash/getrichquick.sh
+++ Bash/getrichquick.sh
@@ -1,6 +1,41 @@
 #1/bin/bash
 
-name=$1
-age=$2
+echo "What is your name?"
 
-echo "Hello, $name, you are $age years old.
+read name
+
+echo "How old are you?"
+
+read age 
+
+echo "Hello, $name, you are $age years old."
+
+sleep 2
+
+echo "Calculating"
+echo "..........."
+sleep 1
+echo "*.........."
+sleep 1
+echo "**........."
+sleep 1
+echo "***........"
+sleep 1
+echo "****......."
+sleep 1
+echo "*****......"
+sleep 1
+echo "******....."
+sleep 1
+echo "*******...."
+sleep 1
+echo "********..."
+sleep 1
+echo "*********.."
+sleep 1
+echo "**********."
+sleep 1
+echo "***********"
+
+getrich=$((( $RANDOM % 15 ) + $age ))
+echo "$name, you will become a millionare when you are $getrich years old"
