74cff393ae0a4cdba35739a3123d39014f92993f
Trystan Williams <trystanwilliams2002@outlook.com>
2025-03-26 09:49:42 -0500

    Update bestdayever.sh

--- Bash/bestdayever.sh
+++ Bash/bestdayever.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+name=$1
+compliment=$2
+
+user=$(whoami)
+whereami=$(pwd)
+date=$(date)
+
+echo "Good Morning $name!"
+sleep 1
+echo "You're looking good today $name!"
+sleep 1
+echo "You have the best $compliment I've seen $name!"
+sleep 2
+
+echo "You are currently logged in as $user and you are in the directory $whereami. Todays date is $date"
