48e7942262b52c125bbf624a91e5b10402fa6c5d
Trystan Williams <trystanwilliams2002@outlook.com>
2025-04-09 08:59:49 -0500

    Update FIM.ps1
    Added some input validation. The code stops after the input is validated, currently working on a fix for this

--- Powershell/FIM.ps1
+++ Powershell/FIM.ps1
@@ -1,13 +1,16 @@
-﻿Write-Host ""
-Write-Host "What would you like to do?"
-Write-Host ""
-Write-Host "    A) Collect new Baseline?"
-Write-Host ""
-Write-Host "    B) Begin monitoring files with saved Baseline?"
-Write-Host ""
+# Inital message to ask user what they would like to do
+$initpromptquestion = "What would you like to do?`n`n     A) Collect new baseline.`n`n     B) Begin monitoring files with saved baseline.`n`nSelect 'A' or 'B'"
+$errormessage = "Invalid entry. Please select 'A' or 'B'."
+$selections = @('A','B')
 
-$response = Read-Host -Prompt "Please enter 'A' or 'B'"
-Write-Host ""
+do {
+    $userinput = Read-Host -Prompt $initpromptquestion
+    switch($userinput) {
+        A { return $userinput }
+        B { return $userinput }
+        default { Write-Host $errormessage -ForegroundColor DarkRed -BackgroundColor White } 
+    }
+} while ($userinput -notcontains $selections)
 
 Function Calculate-File-Hash($filepath) {
     $filehash = Get-FileHash -Path $filepath -Algorithm SHA512
